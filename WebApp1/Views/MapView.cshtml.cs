277af4f6d26dbf76fa87203a331330c13676ab8c
Etienne Du Toit <etiennedutoit69@gmail.com>
2022-06-30 07:25:31 +0200

    Some changes

---
+++ WebApp1/Views/MapView.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp1.Views
+{
+    public class MapViewModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
